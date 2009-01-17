6428baa339a76743a9bd6ef1163861f0a4557883
Jonathan Lange <jml@canonical.com>
2009-01-17 14:13:51 +1100

    Delete unused junk.

--- difftodo.py
+++ difftodo.py
@@ -2,8 +2,6 @@
 
 """A library for extracting TODOs from comments in Python source code."""
 
-from bzrlib import patches
-
 
 def get_comments_from_diff(patches):
     for patch in patches:
@@ -25,18 +23,3 @@ def get_comments_from_diff(patches):
                 current_comment = []
         if len(current_comment) != 0:
             yield ''.join(current_comment)
-
-
-def get_todos_from_diff(patch):
-    for comment in get_comment_blocks_from_diff(patch):
-        if contains_todo(comment):
-            yield make_todo(comment)
-
-
-def get_comment_blocks_from_diff(patch):
-    for chunk in get_chunks(diff):
-        for line in get_lines(chunk):
-            if is_comment(line):
-                yield get_comment_block
-
-
