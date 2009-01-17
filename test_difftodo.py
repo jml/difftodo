8bc68dc163a13f1df2e186517df4ee0d2f5656fb
Jonathan Lange <jml@canonical.com>
2009-01-17 16:06:20 +1100

    Parens might help emacs out a bit.

--- test_difftodo.py
+++ test_difftodo.py
@@ -18,7 +18,7 @@ class TestCommentsFromDiff(TestCase):
     def test_no_comments(self):
         # If the diff doesn't change any comments, then get_comments won't
         # yield anything.
-        diff = """\
+        diff = ("""\
 === modified file 'a'
 --- a	2009-01-17 02:28:16 +0000
 +++ a	2009-01-17 02:28:21 +0000
@@ -32,13 +32,13 @@ class TestCommentsFromDiff(TestCase):
 -    def test_foo(self):
 +    def test_bar(self):
          pass
-"""
+""")
         patch = self.parse_diff(diff)
         self.assertEqual([], list(get_comments_from_diff(patch)))
 
     def test_comment_added(self):
         # If the diff adds a comment, then it's in the stream.
-        diff = """\
+        diff = ("""\
 === modified file 'a'
 --- a	2009-01-17 02:47:22 +0000
 +++ a	2009-01-17 02:47:31 +0000
@@ -48,14 +48,14 @@ class TestCommentsFromDiff(TestCase):
      def test_bar(self):
 +        # This test is going to be awesome.
          pass
-"""
+""")
         patch = self.parse_diff(diff)
         comments = list(get_comments_from_diff(patch))
         self.assertEqual(["# This test is going to be awesome.\n"], comments)
 
     def test_comment_removed(self):
         # If the diff removes a comment, then it's not in the stream.
-        diff = """\
+        diff = ("""\
 === modified file 'a'
 --- a	2009-01-17 02:47:22 +0000
 +++ a	2009-01-17 02:47:31 +0000
@@ -65,13 +65,13 @@ class TestCommentsFromDiff(TestCase):
      def test_bar(self):
 -        # This test is going to be awesome.
          pass
-"""
+""")
         patch = self.parse_diff(diff)
         comments = list(get_comments_from_diff(patch))
         self.assertEqual([], comments)
 
     def test_multiline_comment_added(self):
-        diff = """\
+        diff = ("""\
 === modified file 'a'
 --- a	2009-01-17 02:47:22 +0000
 +++ a	2009-01-17 02:47:31 +0000
@@ -82,7 +82,7 @@ class TestCommentsFromDiff(TestCase):
 +        # This test is going to be awesome.
 +        # So awesome, I cannot tell you how awesome.
          pass
-"""
+""")
         patch = self.parse_diff(diff)
         comments = list(get_comments_from_diff(patch))
         self.assertEqual([
@@ -90,7 +90,7 @@ class TestCommentsFromDiff(TestCase):
             "# So awesome, I cannot tell you how awesome.\n"], comments)
 
     def test_multiple_comments_added(self):
-        diff = """\
+        diff = ("""\
 === modified file 'a'
 --- a	2009-01-17 02:47:22 +0000
 +++ a	2009-01-17 03:09:45 +0000
@@ -103,7 +103,7 @@ class TestCommentsFromDiff(TestCase):
          pass
 +    # Awesome, I tell you.
 
-"""
+""")
         patch = self.parse_diff(diff)
         comments = list(get_comments_from_diff(patch))
         self.assertEqual([
@@ -112,7 +112,7 @@ class TestCommentsFromDiff(TestCase):
             "# Awesome, I tell you.\n"], comments)
 
     def test_comment_modified(self):
-        diff = """\
+        diff = ("""\
 === modified file 'a'
 --- a	2009-01-17 04:04:42 +0000
 +++ a	2009-01-17 04:04:48 +0000
@@ -125,7 +125,7 @@ class TestCommentsFromDiff(TestCase):
          # Line 3
          pass
 
-"""
+""")
         patch = self.parse_diff(diff)
         comments = list(get_comments_from_diff(patch))
         self.assertEqual(["# Line 1\n# Line 2\n# Line 3\n"], comments)
