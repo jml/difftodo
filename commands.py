83cb8143efa6c37cba6bf974cbc151ce556af157
Jonathan Lange <jml@canonical.com>
2009-10-13 13:29:31 +0100

    Update to support new APIs in Bazaar.

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 from StringIO import StringIO
 
 from bzrlib.commands import Command, register_command
-from bzrlib.diff import _get_trees_to_diff, show_diff_trees
+from bzrlib.diff import get_trees_and_branches_to_diff, show_diff_trees
 from bzrlib.option import (
     _parse_revision_str,
     )
@@ -18,14 +18,14 @@ class cmd_todo(Command):
 
     def run(self):
         revision = _parse_revision_str('submit:')
-        old_tree, new_tree, specific_files, extra_trees = (
-            _get_trees_to_diff(None, revision, None, None, apply_view=True))
+        diff_data = get_trees_and_branches_to_diff(
+            None, revision, None, None, apply_view=True)
+        (old_tree, new_tree, old_branch, new_branch, specific_files,
+         extra_trees) = diff_data
         stream = StringIO()
-        diff = show_diff_trees(old_tree, new_tree, stream,
-                               specific_files=specific_files,
-                               external_diff_options=None,
-                               old_label='', new_label='',
-                               extra_trees=extra_trees, using=None)
+        show_diff_trees(
+            old_tree, new_tree, stream, old_label='', new_label='',
+            extra_trees=extra_trees)
         stream.seek(0)
         patches = parse_patches(stream)
         comments = get_comments_from_diff(patches)
